de020561d1a32edb631ef29740baa665b56a1e9f
deepakchanana <deepakdeep1992@gmail.com>
2019-11-02 13:36:28 -0700

    thisischanges

--- 9Am_Batch/src/pkg1/ClassA.java
+++ 9Am_Batch/src/pkg1/ClassA.java
@@ -6,6 +6,7 @@ public class ClassA
     
     public static void main(String[] args) 
     {
+	    Syso("sadfghjawesdfghj");
     	System.out.println("changes done");
 	 ClassA obj=new ClassA();
 	 obj.a=34;
